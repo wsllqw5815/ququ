5e6dc8fb4e9626a1aa834ebe5c38fade751efe3c
weijie_fan <weijie_fan@xinyan.com>
2020-09-14 16:55:03 +0800

    ##hello

---
+++ spring-boot-start/src/main/java/com/nb/main/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.nb.main;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
